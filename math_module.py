ee585b9883c17112882745599456d9a499c5ee18
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-13 23:20:07 +0530

    Add files via upload

---
+++ math_module.py
@@ -0,0 +1,22 @@
+'''
+DATE:12TH NOV 2022
+DAY: MONDAY
+TOPIC: MATH MODULE
+AUTHOR: RAMA BHARGAVI
+'''
+import math
+print(math.ceil(4.6)) #5
+print(math.ceil(-0.5)) #0
+print(math.exp(2)) #7.38905609893065
+print(math.factorial(4)) #24
+print(math.floor(2.6))#2
+print(math.floor(3.4)) #3
+print(math.ceil(3.4)) #4
+a=math.pow(3,3)
+print(a,type(a)) #27.0 float
+print(math.pi) #3.141592653589793
+print(math.pi*2) #6.283185307179586 
+print(math.sqrt(6)) #2.44948974278318
+print(math.sqrt(25)) #5.0
+n=[1,3,5]
+print(math.fsum(n)) #9.0
